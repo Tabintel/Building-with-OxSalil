b8b01027a039e6b3845b2bf5077dbd5717cb813b
Ekemini Samuel <80429893+Tabintel@users.noreply.github.com>
2023-05-09 08:11:46 +0000

    tasks

--- taskThree.js
+++ taskThree.js
@@ -1,15 +1,10 @@
-fetch("https://github.com/public-apis/public-apis")
-.then((response) => {
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  console.log(response);
-  return response.json();
-})
-.then((data) => {
-  console.log(data);
-  // display the data on the page
-})
-.catch((error) => {
-  console.error("Error fetching data:", error);
-});
+fetch("https://api.publicapis.org/entries")
+  .then(response => response.json())
+  .then(data => {
+    // Handle the retrieved data here
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle errors here
+    console.error(error);
+  });
